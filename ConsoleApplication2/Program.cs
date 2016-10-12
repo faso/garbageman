8feb8b12d651c391f470ada2925cbcb767d2e68f
faso <faso@users.noreply.github.com>
2016-10-12 12:19:04 +0300

    aw yeah

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Configuration;
+using Octokit;
+using LibGit2Sharp;
+using System.IO;
+
+namespace garbageman
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("garbageman-package-manager"));
+            var issues = client.Issue.GetAllForRepository("faso", "garbageman").Result.Select(o => new { o.Title, o.Body }).ToList();
+
+            var co = new CloneOptions();
+            co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = "faso", Password = "ribadae13" };
+            try {
+                var url = issues.SingleOrDefault(o => o.Title == args[0]).Body;
+                var name = issues.SingleOrDefault(o => o.Title == args[0]).Title;
+                LibGit2Sharp.Repository.Clone(url, Path.Combine(Environment.CurrentDirectory, "garbage", name), co);
+            }
+            catch
+            {
+                Console.WriteLine("fuck you");
+            }
+        }
+    }
+}
